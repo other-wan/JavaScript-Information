eaf47fc0d7f43535b1c7c5e72b3bf28fa345590b
wan-above <muhammedridwan02@gmail.com>
2022-06-24 11:58:04 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const names = "Tahir, Muhammed Ridwan";
+const height = 180;
+const country = "Nigeria";
+
+const str = `Name: ${names} \nHeight: ${height}cm  \nCountry: ${country}`;
+
+alert(str);
+console.log(str);
